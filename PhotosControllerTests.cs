245f14f5ce3b50a5171510662232408126dc078c
Ekaitz <Ekaitz@PC-EKAITZ>
2025-04-09 18:19:09 +0200

    Agregar archivos de proyecto.

---
+++ PhotosControllerTests.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using Wpf_Ejercicio_4_Ekaitz_Jimenez.Models;
+
+public class PhotosController
+{
+    private HttpClient client;
+
+    //constructor
+    public PhotosController()
+    {
+        client = new HttpClient();
+    }
+
+    // Método de GetAllPhotos
+    public async Task<Photos> GetAllPhotos()
+    {
+        try
+        {
+            Photos photos = new Photos();
+
+            // Simulación de una respuesta de la API con un dato mal
+            // una foto con id incorrecto
+            var responseJson = @"
+                {
+                    'photos': [
+                        {
+                            'id': 0,  // ID incompleto
+                            'earth_date': '2015-05-30',
+                            'img_src': 'http://mars.jpl.nasa.gov/msl-raw-images/proj/msl/r'
+                        }
+                    ]
+                }";
+            photos = JsonConvert.DeserializeObject<Photos>(responseJson);
+
+            // Simulo un caso donde la foto no tiene un ID correcto
+            if (photos.photos.Any(photo => photo.id <= 0))
+            {
+                return null;  // Si hay incompletos, devuelveme null
+            }
+
+            return photos;
+        }
+        catch (Exception ex)
+        {
+            // Si ocurre una excepción, devolvemos null
+            return null;
+        }
+    }
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using Wpf_Ejercicio_4_Ekaitz_Jimenez.Controllers;
+using Xunit;
+using System.Threading.Tasks;
+
+public class PhotosControllerTests
+{
+    [Fact] 
+    public async Task Test_GetAllPhotos_ReturnsValidData()
+    {
+        var controller = new PhotosController();
+
+        // Llamar al método GetAllPhotos de manera asincrona
+        var resultado = await controller.GetAllPhotos();
+
+        // comprobar que el resultado no sea nulo
+        Assert.NotNull(resultado);
+
+        // comprobar que la lista de fotos no esté vacía
+        Assert.NotEmpty(resultado.photos);
+
+        //comptueba que la primera foto tenga un ID válido
+        Assert.True(resultado.photos[0].id > 0);
+    }
+}
